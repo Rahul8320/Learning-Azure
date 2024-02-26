617e511bb54ca35d6972e70afe5100b70093efce
Rahul Dey <rahuldey832000@gmail.com>
2024-02-26 16:13:02 +0530

    Fixed blob trigger issue

--- HttpTriggerFunction/Functions/ProcessFunction.cs
+++ HttpTriggerFunction/Functions/ProcessFunction.cs
@@ -7,11 +7,15 @@ public class ProcessFunction(ILogger<ProcessFunction> logger)
 {
     private readonly ILogger<ProcessFunction> _logger = logger;
 
-    [Function(nameof(ProcessFunction))]
-    public async Task Run([BlobTrigger("pdf-storage/{name}", Connection = "BlobStorageConnectionString")] Stream stream, string name)
+    [Function("ProcessFunction")]
+    public async Task Run(
+        [BlobTrigger("pdf-storage/{name}",
+        Connection = "BlobStorageConnectionString")]
+        Stream stream,
+        string name)
     {
         using var blobStreamReader = new StreamReader(stream);
         var content = await blobStreamReader.ReadToEndAsync();
-        _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {content}");
+        _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {"content"}");
     }
 }
