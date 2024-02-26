d593fcb4b42768537cc6f1b633011eee19eda702
Rahul Dey <rahuldey832000@gmail.com>
2024-02-26 10:49:01 +0530

    Create Blog Trigger function

---
+++ HttpTriggerFunction/Functions/ProcessFunction.cs
@@ -0,0 +1,17 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace HttpTriggerFunction.Functions;
+
+public class ProcessFunction(ILogger<ProcessFunction> logger)
+{
+    private readonly ILogger<ProcessFunction> _logger = logger;
+
+    [Function(nameof(ProcessFunction))]
+    public async Task Run([BlobTrigger("pdf-storage/{name}", Connection = "BlobStorageConnectionString")] Stream stream, string name)
+    {
+        using var blobStreamReader = new StreamReader(stream);
+        var content = await blobStreamReader.ReadToEndAsync();
+        _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {content}");
+    }
+}
