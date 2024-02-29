e8d7d85715be37c5c4000042bddfa2801f884e0b
Rahul Dey <rahuldey832000@gmail.com>
2024-02-29 12:21:14 +0530

    Read blob file content

--- HttpTriggerFunction/Functions/ProcessFunction.cs
+++ HttpTriggerFunction/Functions/ProcessFunction.cs
@@ -14,8 +14,9 @@ public class ProcessFunction(ILogger<ProcessFunction> logger)
         Stream stream,
         string name)
     {
-        _logger.LogInformation($"C# Blob trigger function Processed blob Name: {name}");
         using var blobStreamReader = new StreamReader(stream);
         var content = await blobStreamReader.ReadToEndAsync();
+
+        _logger.LogInformation($"C# Blob trigger function Processed blob \n Name: {name}\n Content: {content}");
     }
 }
